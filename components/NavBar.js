553bb3b3312f10d529ddcbe125f4d95e9024f6f8
chollid <chrisuga@me.com>
2021-12-28 21:12:14 -0700

    push front page:

---
+++ components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div
+      style={{
+        height: "100px",
+        opacity: "0.5",
+      }}
+    ></div>
+  );
+}
+
+export default NavBar;
